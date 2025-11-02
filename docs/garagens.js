deb765b46281ea5b7eda1caa031d511481354d1b
monitoramentov3lan <monitoramentov3lan@gmail.com>
2025-11-02 17:30:29 -0300

    Add files via upload

---
+++ docs/garagens.js
@@ -0,0 +1,32 @@
+// Arquivo: garagens.js
+
+// IMPORTANTE:
+// Esta lista DEVE conter os nomes exatos de todas as suas 26 garagens.
+// O nome deve ser igual ao NOME_GARAGEM do config.ini de cada bot.
+
+const GARAGENS = [
+    "TCM_FORQUILHA",
+    "ESTRELA",
+    "RATRANS",
+    "TCC",
+    "RIO NEGRO",
+    "TAGUATUR",
+    "REQUINTE",
+    "VIA-SL",
+    "TSC",
+    "MATOS",
+    "TCM-RIBAMAR",
+    "TCM-RAPOSA",
+    "TCV",
+    "PREMIUM",
+    "VIPER",
+    "TPG",
+    "PRIMOR",
+    "TDI",
+    "UNIÃO",
+    "SPEED",
+    "ZURIQUE",
+    "PLANETA",
+    "INOVARE",
+    // ... adicione todas as 26 aqui
+];
